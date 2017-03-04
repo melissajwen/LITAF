12930d08285a66066768c236d556af8a4aefa3a8
Corvyn Kusuma <corvyn.kusuma@gmail.com>
2017-03-04 10:59:23 -0800

    Added textAnalytics.js and functionality to analyze keyword matches

---
+++ textAnalytics.js
@@ -0,0 +1,29 @@
+function compareKeywords(userArray, teacherArray){
+    var newWordCount = 0;
+    var newWords = [];
+    var matchedWords = [];
+    var matchedWordCount = 0;
+
+    for (var i = 0; i < userArray.length; i++){
+        var matched = false;
+        for (var j = 0; j < teacherArray.length; j++){
+            if (userArray[i] == teacherArray[j]){
+                matched = true;
+                matchedWords.push(userArray[i]);
+                matchedWordCount++;
+                break;
+            }
+        }
+        if (matched == false){
+            newWordCount++;
+            newWords.push(userArray[i]);
+        }
+    }
+    return {
+        newWords: newWords,
+        newWordCount: newWordCount,
+        matchedWords: matchedWords,
+        matchedWordCount: matchedWordCount
+    };
+}
+
