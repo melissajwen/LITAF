6512042f115ffd55117616a97035b32c1d051713
Corvyn Kusuma <corvyn.kusuma@gmail.com>
2017-03-05 08:14:50 -0800

    Upload Photo and Fade Effects (NOT COMPLETE YET)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Spliced Comma'}); // TODO: Update this when we figure out a cool name
+  res.render('index', { title: 'LitAF'}); // TODO: Update this when we figure out a cool name
 });
 
 module.exports = router;
