903f29acc405e5eefcd84374d9ea77f3ec8fdfc0
melissajwen <wenm@usc.edu>
2017-03-04 16:32:30 -0800

    compiled all javascript files

--- content.js
+++ content.js
@@ -61,3 +61,61 @@ function sentimentAnalysis() {
 
     return false;
 }
+
+function compareKeywords(userArray, teacherArray){
+    var newWordCount = 0;
+    var newWords = [];
+    var matchedWords = [];
+    var matchedWordCount = 0;
+
+    for (var i = 0; i < userArray.length; i++){
+        var matched = false;
+        for (var j = 0; j < teacherArray.length; j++){
+            if (userArray[i] == teacherArray[j]){
+                matched = true;
+                matchedWords.push(userArray[i]);
+                matchedWordCount++;
+                break;
+            }
+        }
+        if (matched == false){
+            newWordCount++;
+            newWords.push(userArray[i]);
+        }
+    }
+    var percentageMatched = matchedWordCount/teacherArray.size;
+    return {
+        newWords: newWords,
+        newWordCount: newWordCount,
+        matchedWords: matchedWords,
+        matchedWordCount: matchedWordCount
+        percentageMatched: percentageMatched
+    };
+}
+
+function compare(teacherInput) {
+
+    var params = {
+        // Request parameters
+        "s1": $("#text").val(),
+        "s2": teacherInput,
+    };
+
+    $.ajax({
+        url: "https://westus.api.cognitive.microsoft.com/academic/v1.0/similarity?" + "s1=" + params.s1 + "&s2=" + params.s2,
+        headers: {
+            "Ocp-Apim-Subscription-Key": "963f6c662dc347279cf7d8e11f08ec1b",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        type: "GET",
+        // Request body
+        data: {body},
+    })
+        .done(function(data) {
+            return data;
+        })
+        .fail(function() {
+            alert("error");
+        });
+});
